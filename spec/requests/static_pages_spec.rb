bedf64bd7e9789254626055fdd431d9d9cda1f5e
marmit <mecrobmarmit@yahoo.com>
2012-06-26 14:59:16 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Creative Art and Drawing Studio" }
+
+  describe "Home page" do
+    it "should have the content 'Creative Art and Drawing Studio'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => base_title)
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                  :text => "#{base_title} | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                  :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                  :text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                  :text => "#{base_title} | Contact Us")
+    end
+  end
+end
